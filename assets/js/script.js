2602bc8899b18011d6d912aed712bd92cf1f9f0a
Willams Silva <willamssilvainfo@gmail.com>
2023-10-23 11:59:50 -0300

    Página de Vendas

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+let btn = document.querySelector("#back-to-top");
+
+btn.addEventListener("click", function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+window.addEventListener("scroll", function() {
+  if (window.scrollY > 0) {
+    btn.style.display = "block"; 
+  } else {
+    btn.style.display = "none";
+  }
+});
